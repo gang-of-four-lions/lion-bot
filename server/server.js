400ebc31e2cef14254a45ca218398ea8402b419e
Vladimir Logachev <doit@keemail.me>
2017-02-27 07:37:05 +0700

    enchanced test for help command
    now checking also ephemeral response type

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
@@ -18,37 +18,40 @@ const NUM_ITEMS = 5;
 //All slash commands are sent as posts by default
 //I think we should setup the routes in /api/ so it won't get in the way of later expansion
 app.post('/api/*', (req, res) => {
-    if(req.body.token !== process.env.TOKEN){
-        res.end("Invaild token.");
-        return;
-    } //Validate token
+  if (req.body.token !== process.env.TOKEN) {
+    res.end("Invaild token.");
+    return;
+  } //Validate token
 
-    //Here we will setup the response JSON object probably with a seperate function
-    const RESPONSE_URL = req.body.response_url;
-    res.status(200); //We must include this with all JSON object responses
-    if (req.body.command === '/lion-bot') {
-        if (req.body.text === 'help') {
-            let helpText = {
-                text: [`/lion-bot help:`,
-                        `*/lion-bot:* shows a random item`,
-                        `*/lion-bot [id]:* shows the item with the specified id (0 to ${NUM_ITEMS - 1})`,
-                        `*/lion-bot filtered:* shows a SFW random item`,
-                        `*/lion-bot filtered [id]:* shows the SFW item with the specified id (0 to ${NUM_ITEMS - 1})`,
-                        `*/lion-bot help:* shows this text`].join('\n'),
-                
-            }
-            res.json(helpText);
-             return;
-        }
+  //Here we will setup the response JSON object probably with a seperate function
+  const RESPONSE_URL = req.body.response_url;
+  res.status(200); //We must include this with all JSON object responses
+  if (req.body.command === '/lion-bot') {
+    if (req.body.text === 'help') {
+      let helpText = {
+        response_type: `ephemeral`, // only 1 user will see the response
+        text: [`/lion-bot help:`,
+          `*/lion-bot:* shows a random item`,
+          `*/lion-bot [id]:* shows the item with the specified id (0 to ${NUM_ITEMS - 1})`,
+          `*/lion-bot filtered:* shows a SFW random item`,
+          `*/lion-bot filtered [id]:* shows the SFW item with the specified id (0 to ${NUM_ITEMS - 1})`,
+          `*/lion-bot help:* shows this text`].join('\n'),
+
+      }
+      res.json(helpText);
+      return;
     }
-    res.json({text: 'error: no command'});
-    //res.send(theResponse);
+  }
+  res.json({
+    text: 'error: no command'
+  });
+//res.send(theResponse);
 });
 
 //use standard get '/' to deliver the landing page
 app.get('/', (req, res) => {
-	//res.end('lion-bot says hello!');
-	res.status(200).sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+  //res.end('lion-bot says hello!');
+  res.status(200).sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
 module.exports = app;
--- test/server.test.js
+++ test/server.test.js
@@ -3,18 +3,19 @@ const app = require('../server/server.js');
 const request = require('supertest')
 
 describe('server', function() {
-  it('should return help text for the help command', function(done) {
+  it('should return help text for the help command, with ephemeral response type', function(done) {
     request(app)
       .post('/api/anything')
       .send('text=help')
       .send('command=/lion-bot')
       .expect(function(res) {
         expect(res.body.text).to.have.string('help');
+        expect(res.body.response_type).to.equal('ephemeral');
       })
       .end(done);
   });
 
-  it('GET "/" should return the landing page', function(done){
+  it('GET "/" should return the landing page', function(done) {
     request(app)
       .get('/')
       .expect(200)
